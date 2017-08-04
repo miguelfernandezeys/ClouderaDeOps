73afc1102d6b4dfd272472d1b6e376e6790cc875
miguelfernandezeys <miguel.fernandez@eys.co>
2017-08-03 23:07:29 -0500

    Update Clouderamanager.sh

--- Clouderamanager.sh
+++ Clouderamanager.sh
@@ -24,33 +24,43 @@ cat /etc/selinux/config
 #Configuracion de hosts
 echo "Defina las direcciones ip  privadas de los nodos del cluster"
 sleep 4
+
 echo "Ip-1"
+
 read IP1
-sleep 4
+
 echo "Ip-2"
+
 read IP2
-sleep 4
+
 echo "Ip-3"
+
 read IP3
-sleep 4
+
 echo "Ip-4"
+
 read IP4
-sleep 4
+
 echo "Ip-5"
+
 read IP5
+
 arrIN1=(${IP1//./ })
 arrIN2=(${IP2//./ })
 arrIN3=(${IP3//./ })
 arrIN4=(${IP4//./ })
 arrIN5=(${IP5//./ })
+
 echo "$IP1 			ip-${arrIN1[0]}-${arrIN1[1]}-${arrIN1[2]}-${arrIN1[3]}$DIR" >> /etc/hosts
 echo "$IP2 			ip-${arrIN2[0]}-${arrIN2[1]}-${arrIN2[2]}-${arrIN2[3]}$DIR" >> /etc/hosts
 echo "$IP3 			ip-${arrIN3[0]}-${arrIN3[1]}-${arrIN3[2]}-${arrIN3[3]}$DIR" >> /etc/hosts
 echo "$IP4 			ip-${arrIN4[0]}-${arrIN4[1]}-${arrIN4[2]}-${arrIN4[3]}$DIR" >> /etc/hosts
 echo "$IP5 			ip-${arrIN5[0]}-${arrIN5[1]}-${arrIN5[2]}-${arrIN5[3]}$DIR" >> /etc/hosts
+
 cat /etc/hosts
 #Instalacion de repo MariaDB 10.0
 > /etc/yum.repos.d/MariaDB.repo
+
 echo "
 # MariaDB 10.0 CentOS repository list - created 2017-08-04 03:32 UTC
 # http://downloads.mariadb.org/mariadb/repositories/
@@ -60,11 +70,16 @@ baseurl = http://yum.mariadb.org/10.0/centos7-amd64
 gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
 gpgcheck=1
 " >> /etc/yum.repos.d/MariaDB.repo
+
 cat /etc/yum.repos.d/MariaDB.repo
 #Instalacion de MariaDB-server y MariaDB-client
+
 yum repolist
+
 sleep 4
+
 yum -y install MariaDB-server MariaDB-client
+
 systemctl enable mysql
 systemctl start mysql
 systemctl status mysql
